9b1fbdbb7eb0375b58a44cb39d787b4531a99d98
gandlaf21 <gandlaf@proton.me>
2025-05-30 14:52:40 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../style.css";
+  const { children } = $props();
+
+</script>
+
+<div class="flex w-screen justify-center p-2">
+<div class="flex max-w-4xl">
+
+  {@render children()}
+</div>
+</div>
